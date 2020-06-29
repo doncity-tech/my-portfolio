f64fd89a7729c1e63fa3bc02b9de029ba4b4814e
doncity-tech <princewill.opara@outlook.com>
2020-06-29 22:19:55 +0100

    added starter code

---
+++ js/third-party-cdn.js
@@ -0,0 +1,7 @@
+
+/* Bootstrap CDNs */
+document.write(`<script src="bootstrap-js/popper.js"></script>`);
+document.write(`<script src="bootstrap-js/jquery.js"></script>`);
+document.write(`<script src="bootstrap-js/bootstrap.js"></script>`);
+
+
