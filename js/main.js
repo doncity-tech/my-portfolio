44c6ad8195a882a01b4189fe1614eaa8b7ae2a78
doncity-tech <princewill.opara@outlook.com>
2020-07-06 00:59:16 +0100

    all good

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-document.write('Working fine');
+
+let toggleBtn = document.querySelector('.dark-mode-toggle');
+toggleBtn.addEventListener('click', e => {
+  let classes = e.target.parentNode.classList;
+  classes.remove('justify-content-start');
+  classes.add('justify-content-end');
+});
