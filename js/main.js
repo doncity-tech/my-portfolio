410f9aedd0d7ded336a666d2427a53fe1c899c99
doncity-tech <princewill.opara@outlook.com>
2020-07-06 01:09:43 +0100

    all good

--- js/main.js
+++ js/main.js
@@ -2,6 +2,23 @@
 let toggleBtn = document.querySelector('.dark-mode-toggle');
 toggleBtn.addEventListener('click', e => {
   let classes = e.target.parentNode.classList;
-  classes.remove('justify-content-start');
-  classes.add('justify-content-end');
+  if (classes[2] === 'justify-content-start') {
+    classes.remove('justify-content-start');
+    classes.add('justify-content-end');
+  } else {
+    classes.remove('justify-content-end');
+    classes.add('justify-content-start');
+  }
+});
+
+let toggleBtnDiv = document.querySelector('.dark-mode-btn');
+toggleBtnDiv.addEventListener('click', e => {
+  let classes = e.target.classList;
+  if (classes[2] === 'justify-content-start') {
+    classes.remove('justify-content-start');
+    classes.add('justify-content-end');
+  } else {
+    classes.remove('justify-content-end');
+    classes.add('justify-content-start');
+  }
 });
